97609d1dbc7b3b51d97c0a12db497744c12635c8
Mriyam Tamuli <mbtamuli@gmail.com>
2024-05-16 19:12:15 +0530

    Fix all errors, all errors on the WORLD!!!!
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- app/query_chain.py
+++ app/query_chain.py
@@ -1,10 +1,13 @@
-from langchain_community.embeddings import BedrockEmbeddings
 from langchain_aws import ChatBedrock
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-embeddings_model_id = "amazon.titan-embed-text-v1"
+embeddings_model_id = "amazon.titan-text-express-v1"
 region_name = "us-west-2"
 
 bedrock_embedding = BedrockEmbeddings(
@@ -17,24 +20,40 @@ anthropic_claude_llm = ChatBedrock(
     model_id="anthropic.claude-3-haiku-20240307-v1:0"
 )
 
-TEMPLATE = """Given an input, create syntactically correct PHP code. Don't provide any explanation with the code.
-
-Use the following format:
+TEMPLATE = """Given an input, create syntactically correct PHP code. Don't provide any explanation with the code. Use the following format:
 
 Question: "Question here"
 Answer: "Answer here"
 
-Question: Write a Drupal functional test PHP function for the following
+Answer the question based only on the following context:
+{context}
+
+Question: Write Drupal functional test for the following
 {question}"""
 
 custom_prompt_template = PromptTemplate(
     input_variables=["context", "question"], template=TEMPLATE
 )
 
+# Load the data and split it into chunks
+loader = DirectoryLoader('./ragdata')
+documents = loader.load()
+
+# Split document into chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
+)
+docs = text_splitter.split_documents(documents)
+
+# Load the embeddings into Chroma in-memory vector store
+vectorstore = Chroma.from_documents(docs, embedding=bedrock_embedding)
+vectorstore_retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
+
 model = anthropic_claude_llm
 prompt = ChatPromptTemplate.from_template(TEMPLATE)
 chain = (
     {
+        "context": vectorstore_retriever,
         "question": RunnablePassthrough()
     }
     | prompt
@@ -46,6 +65,7 @@ chain = (
 def question_chain(question):
     chain = (
         {
+            "context": vectorstore_retriever,
             "question": RunnablePassthrough()
         }
         | prompt
