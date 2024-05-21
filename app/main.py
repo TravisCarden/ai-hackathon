2a8a153eb6fadb80150fd1ec6c1204901c159fe6
Mriyam Tamuli <mbtamuli@gmail.com>
2024-05-21 17:06:40 +0530

    improvements
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ logging.basicConfig(filename='ai-hackathon.log')
 logger.setLevel(os.environ.get('LOG_LEVEL', logging.INFO))
 
 def ask_question(question):
-    logger.info("Invoking the AI")
     with st.spinner('Wait for it...'):
         return ai.invoke(question)
 
@@ -34,33 +33,21 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.code(message["content"], language=message.get("type"))
 
-#prompt = st.chat_input("Drupal module routing file contents")
+prompt = st.chat_input("Drupal module routing file contents")
 
-
-uploaded_file = st.file_uploader("Choose a file")
-
-if uploaded_file is not None:
-    # To convert to a string based IO:
-    string_input = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
-
-
+if prompt:
      # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": string_input, "type": "yaml"})
+    st.session_state.messages.append({"role": "user", "content": prompt, "type": "yaml"})
+
     # Display user message in chat message container
     with st.chat_message("user"):
-        st.code(string_input, language="yaml")
+        st.code(prompt, language="yaml")
+
     ## Display assistant response in chat message container
-    full_response = ""
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
-        response_list = []
-        response = extract_answer(ask_question(string_input))
-        response_list.append(response)
+        logger.debug("User prompt " + json.dumps(prompt))
+        response = extract_answer(ask_question(prompt))
+        message_placeholder.code(response, language='php')
 
-        for response in response_list:
-            full_response += response
-            message_placeholder.code(full_response, language='php')
-
-    st.session_state.messages.append({"role": "assistant", "content": full_response, "type": "php"})
-
-logging.basicConfig(filename='myapp.log', level=logging.INFO)
+    st.session_state.messages.append({"role": "assistant", "content": response, "type": "php"})
